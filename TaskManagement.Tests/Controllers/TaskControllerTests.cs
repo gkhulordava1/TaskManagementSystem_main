9b978f65ee21b8ce78aa4e29aa45146e77f32b3a
Goga Khulordava <Goga Khulordava@DESKTOP-OL22IV5>
2024-12-02 02:28:51 +0400

    Add project files.

---
+++ TaskManagement.Tests/Controllers/TaskControllerTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Moq;
+using TaskManagement.Controllers;
+using TaskManagement.DTOs;
+using TaskManagement.Services.Interfaces;
+
+namespace TaskManagement.Tests.Controllers;
+
+public class TaskControllerTests
+{
+    [Fact]
+    public async Task GetTasks_Returns_OkResult_With_Tasks()
+    {
+
+        var mockTaskService = new Mock<ITaskService>();
+        var mockMemoryCache = new Mock<IMemoryCache>();
+
+        var tasks = new List<TaskDTO>
+            {
+                new TaskDTO { Id = 1, Name = "Task 1", Description = "Description 1" },
+                new TaskDTO { Id = 2, Name = "Task 2", Description = "Description 2" }
+            };
+        mockTaskService.Setup(service => service.GetAllTasks()).ReturnsAsync(tasks);
+
+        var controller = new TaskController(mockTaskService.Object, mockMemoryCache.Object);
+
+        var result = await controller.GetTasks();
+
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var taskList = Assert.IsAssignableFrom<IEnumerable<TaskDTO>>(okResult.Value);
+        Assert.Equal(2, taskList.Count());
+
+    }
+
+    [Fact]
+    private static async Task GetTaskById_Returns_OkResult_With_Task()
+    {
+        int taskId = 1;
+        var mockTaskService = new Mock<ITaskService>();
+        var mockMemoryCache = new Mock<IMemoryCache>();
+
+        var task = new TaskDTO { Id = taskId, Name = "Task 1", Description = "Description 1" };
+        mockTaskService.Setup(service => service.GetTaskById(taskId)).ReturnsAsync(task);
+
+        var controller = new TaskController(mockTaskService.Object, mockMemoryCache.Object);
+
+        var result = await controller.GetTaskById(taskId);
+
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var taskResult = Assert.IsAssignableFrom<TaskDTO>(okResult.Value);
+        Assert.Equal(taskId, taskResult.Id);
+        Assert.Equal(task.Name, taskResult.Name);
+        Assert.Equal(task.Description, taskResult.Description);
+    }
+
+}
---
+++ TaskManagement.Tests/Repositories/TaskRepositoryTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using TaskManagement.Data;
+using TaskManagement.Repositories.Implementations;
+using Task = System.Threading.Tasks.Task;
+
+namespace TaskManagement.Tests.Repositories;
+
+
+public class TaskRepositoryTests
+{
+    private DbContextOptions<AppDbContext> _options;
+
+    public TaskRepositoryTests()
+    {
+        _options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+
+        using (var context = new AppDbContext(_options))
+        {
+            context.Tasks.AddRange(new List<Models.Task>
+                {
+                    new Models.Task { Id = 1, Name = "Task 1", Description = "Description 1" },
+                    new Models.Task { Id = 2, Name = "Task 2", Description = "Description 2" }
+                });
+            context.SaveChanges();
+        }
+    }
+
+    [Fact]
+    public async Task GetAllTasks_Returns_All_Tasks()
+    {
+      
+        using (var context = new AppDbContext(_options))
+        {
+            var repository = new TaskRepository(context);
+
+            var tasks = await repository.GetAllTasks();
+
+            Assert.NotNull(tasks);
+            Assert.Equal(2, tasks.Count()); 
+        }
+    }
+}
+
